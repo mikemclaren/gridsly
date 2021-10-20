690a44bbb3e27d34cff90342aec3b6059cf1b241
Mike Mclaren <rastalinmitomo@gmail.com>
2021-10-20 17:41:26 -0500

    Add back like everything lol

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,10 @@ export interface MapProps {
 
 }
 
-// const Map = dynamic<MapProps>(() => import('./components/Map/Map'), {
-//   ssr: false,
-//   loading: () => <p>...</p>
-// })
+const Map = dynamic<MapProps>(() => import('../components/Map/Map'), {
+  ssr: false,
+  loading: () => <p>...</p>
+})
 
 export default function Home() {
   return (
@@ -20,7 +20,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* <Map /> */}
+      <Map />
     </Box>
   )
 }
